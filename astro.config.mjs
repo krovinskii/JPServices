2fe49a624f3eb4210eb9c14fb89eff4225d2aa11
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-04-11 12:01:54 -0500

    minify astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
     image: {
@@ -8,4 +10,6 @@ export default defineConfig({
             entrypoint: "astro/assets/services/sharp",
         },
     },
+
+    integrations: [compress()],
 });
